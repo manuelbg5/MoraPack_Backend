6b4e423cbaa1ca0d37e468ce900e606e6a581573
Manuel Barrantes <a20182619@pucp.edu.pe>
2025-10-21 03:41:48 -0500

    nuevo

---
+++ src/main/java/pe/edu/pucp/morapack/MoraPackBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.morapack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoraPackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoraPackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/pucp/morapack/MoraPackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.morapack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoraPackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
